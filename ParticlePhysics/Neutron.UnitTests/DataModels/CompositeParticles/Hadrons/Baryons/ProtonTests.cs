df4ac5da20133e230b5649153ccf37236472b99d
Kevin Kabatra <kevin@lsretail.com>
2022-01-01 17:16:46 -0500

    Proton is a Baryon now
    Followed suit with what I did for the Neutron.

--- ParticlePhysics/Neutron.UnitTests/DataModels/CompositeParticles/Hadrons/Baryons/ProtonTests.cs
+++ ParticlePhysics/Neutron.UnitTests/DataModels/CompositeParticles/Hadrons/Baryons/ProtonTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace SubatomicParticles.UnitTests.DataModels.CompositeParticles.Hadrons.Baryons
 {
+    using System.Linq;
     using SubatomicParticles.DataModels.CompositeParticles.Hadrons.Baryons;
     using Xunit;
 
@@ -11,8 +12,15 @@
             var proton = new Proton();
 
             Assert.NotNull(proton);
+            Assert.Collection(proton.Quarks,
+                firstQuark => Assert.Equal(Proton.ConstantComposition.ToList()[0], firstQuark),
+                secondQuark => Assert.Equal(Proton.ConstantComposition.ToList()[1], secondQuark),
+                thirdQuark => Assert.Equal(Proton.ConstantComposition.ToList()[2], thirdQuark)
+            );
             Assert.Equal(Proton.ConstantChargeType, proton.Charge);
+            Assert.Equal(Proton.ConstantChargeValue, proton.ChargeValue);
             Assert.Equal(Proton.ConstantMassInKilograms, proton.MassInKilograms);
+            Assert.Equal(Proton.ConstantMassInElectronVolts, proton.MassInElectronVolts);
         }
 
         [Fact]
--- ParticlePhysics/SubatomicParticles/DataModels/CompositeParticles/Hadrons/Baryons/Proton.cs
+++ ParticlePhysics/SubatomicParticles/DataModels/CompositeParticles/Hadrons/Baryons/Proton.cs
@@ -1,21 +1,31 @@
 ﻿namespace SubatomicParticles.DataModels.CompositeParticles.Hadrons.Baryons
 {
     using System;
+    using System.Collections.Generic;
     using Constants;
+    using ElementaryParticles.Quarks;
+    using Interfaces.ElementaryParticles.Quarks;
 
     /// <summary>
     ///     The proton is a subatomic particle which has a positive electric charge and a mass slightly less than that of a neutron.
     /// <para>See the following article for more information:</para>
     /// <para>1. <a href="https://en.wikipedia.org/wiki/Proton">Wikipedia: Proton</a></para>
     /// </summary>
-    public class Proton : CompositeParticle
+    public class Proton : Baryon
     {
+        public static readonly ICollection<IQuark> ConstantComposition = new List<IQuark>
+        {
+            new UpQuark(),
+            new UpQuark(),
+            new DownQuark()
+        };
+
         public const ChargeType ConstantChargeType = ChargeType.Positive;
         public const double ConstantChargeValue = 1d;
         public static readonly double ConstantMassInKilograms = 1.67262192369 * Math.Pow(10, -27);
         public const double ConstantMassInElectronVolts = 938.27208816;
 
-        public Proton() : base(ConstantChargeType, ConstantChargeValue, ConstantMassInKilograms, ConstantMassInElectronVolts)
+        public Proton() : base(ConstantComposition, ConstantMassInKilograms, ConstantMassInElectronVolts)
         {
         }
     }
