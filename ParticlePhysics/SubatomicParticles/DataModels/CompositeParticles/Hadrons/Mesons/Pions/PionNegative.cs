d704784c2ed614503ea365f0181fc6882da81c2b
Kevin Kabatra <kevin@lsretail.com>
2022-01-16 21:39:12 -0500

    Pion Negative tests, also can create Pion Negative by specifying Quarks and Gluons

--- ParticlePhysics/SubatomicParticles/DataModels/CompositeParticles/Hadrons/Mesons/Pions/PionNegative.cs
+++ ParticlePhysics/SubatomicParticles/DataModels/CompositeParticles/Hadrons/Mesons/Pions/PionNegative.cs
@@ -2,8 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Constants;
     using ElementaryParticles.Quarks;
+    using Interfaces.ElementaryParticles;
     using Interfaces.ElementaryParticles.Quarks;
 
     /// <inheritdoc cref="Pion"/>
@@ -22,6 +24,17 @@
 
         public PionNegative() : base(ConstantComposition, Meson.ConstantGluons, ConstantMassInElectronVolts, ConstantAntiparticleType)
         {
+        }
+
+        public PionNegative(ICollection<IQuark> quarks, ICollection<IGluon> gluons) : base(quarks, gluons, ConstantMassInElectronVolts, ConstantAntiparticleType)
+        {
+            var numberOfDownQuarks = quarks.OfType<DownQuark>().Count();
+            var numberOfAntiUpQuarks = quarks.OfType<AntiUpQuark>().Count();
+
+            if (numberOfDownQuarks != 1 && numberOfAntiUpQuarks != 1)
+            {
+                throw new ArgumentException($"A Negative Pion requires one (1) Down Quark and two (1) AntiUp Quarks. This Negative Pion contains {numberOfDownQuarks} Down Quarks and {numberOfAntiUpQuarks} AntiUp Quarks.");
+            }
         }
     }
 }
