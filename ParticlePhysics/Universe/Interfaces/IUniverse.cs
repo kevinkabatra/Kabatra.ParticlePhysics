b29ce64937d80a9c05748f8349daeacd3864e49e
Kevin Kabatra <kevin@lsretail.com>
2022-01-17 13:22:25 -0500

    Work in progress.
    Didn't have much time for this session. But I am thinking that I will make an instance method on the interface that will return the expected Universe. Feels gross, but it might be "the best of our bad plans". Currently writing what feels like an essay in the comments, and so I have put a bunch of notes to remember what my current thoughts are on this, will fill it all in later on.

--- ParticlePhysics/Universe/Interfaces/IUniverse.cs
+++ ParticlePhysics/Universe/Interfaces/IUniverse.cs
@@ -13,6 +13,44 @@
     public interface IUniverse
     {
         /// <summary>
+        ///     Gets the Universe.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        ///     I am going to rant to explain my problem, and my awful solution to that problem. I like the idea
+        /// of the Universe following the Singleton pattern, but Singletons really cause a huge amount annoyance
+        /// when it comes to isolating data during parallel execution of unit tests. But if we think about this
+        /// in a physical sense, there is only one universe (except in multiverse theories, and I am not coding
+        /// for that, well I guess this change is going to make a multiverse... oh boy).
+        /// </para>
+        /// <para>
+        ///     My current struggle is that when my tests execute in parallel there are many particles popping
+        /// in and out of existence, and it is impossible to ask the Universe if it only contains n number of
+        /// any particular subatomic particle, because another test could have easily created those same particles.
+        /// To work around this a lot of my tests have just validated that the Universe contains particles of
+        /// a particular type, and not validated the quantity of them. I think this is sufficient for most unit
+        /// testing but I have a scenario where it is not, and if I do not solve this problem I will continue
+        /// to introduce an issue back into the system.
+        /// </para>
+        /// <para>
+        ///     ToDo: explain the bug, how it was repaired, but only for Neutrons, and how I reintroduced when I looked at Protons and liked that implementation better
+        /// </para>
+        /// <para>
+        ///     ToDo: Explain how to resolve this using an isolated universe. Explain how it can be created using Dependency Injection
+        /// </para>
+        /// <para>
+        ///     ToDo: Explain that interfaces cannot have static methods and static methods cannot implement an interface method
+        ///     ToDo: Link to stackoverflow: https://stackoverflow.com/questions/9415257/how-can-i-implement-static-methods-on-an-interface
+        ///     ToDo: This method will be implemented on all data models and in the case of Universe, it is just a wrapper for Universe.GetOrCreateInstance()
+        /// </para>
+        ///     ToDo: Explain that this is still gross, as you will have to create a new Universe() in order to call .Get() which seems silly because new Universe() would have already given me a Universe.
+        ///     ToDo: Almost makes me want to rip out the Singleton aspect of the Universe, but that then complicates how each new particle can add itself to the Universe upon creation.
+        /// <para>
+        /// </para>
+        /// </remarks>
+        public IUniverse Get { get; }
+
+        /// <summary>
         ///     Current epoch of the Universe, when the epoch is updated the temperature and time is adjusted.
         /// </summary>
         public Epoch Epoch { get; }
